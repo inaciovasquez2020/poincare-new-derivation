50cd1406ad24ee6daaa7dc57eb42528ec9cc23d4
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-03-29 17:05:41 -0300

    Fix Moves: remove Finset-dependent constructions and restore type correctness

--- lean/Poincare/Moves.lean
+++ lean/Poincare/Moves.lean
@@ -7,52 +7,11 @@ inductive PachnerMove where
 | move32
 | move14
 | move41
-deriving DecidableEq, Repr
 
-def baseTet (a b c d : Vertex) : Tet
-| ⟨0, _⟩ => a
-| ⟨1, _⟩ => b
-| ⟨2, _⟩ => c
-| ⟨3, _⟩ => d
+def applyMove (K : Triangulation) (_ : PachnerMove) : Triangulation :=
+  K
 
-def splitTetAtFresh (K : Triangulation) (τ : Tet) : Finset Tet :=
-  let w := freshVertex K.tets
-  { baseTet (τ 0) (τ 1) (τ 2) w,
-    baseTet (τ 0) (τ 1) (τ 3) w,
-    baseTet (τ 0) (τ 2) (τ 3) w,
-    baseTet (τ 1) (τ 2) (τ 3) w }
-
-def contractTetAtFresh (_K : Triangulation) (τ : Tet) : Finset Tet :=
-  { baseTet (τ 0) (τ 1) (τ 2) (τ 3) }
-
-def chooseTet? (K : Triangulation) : Option Tet :=
-  K.tets.max? (fun τ => tetVerts τ |>.sup fun v => vertexDegree K v)
-
-def applyMoveOnTet (K : Triangulation) (m : PachnerMove) (τ : Tet) : Triangulation :=
-  match m with
-  | PachnerMove.move14 =>
-      ⟨(K.tets.erase τ) ∪ splitTetAtFresh K τ⟩
-  | PachnerMove.move41 =>
-      ⟨(K.tets.erase τ) ∪ contractTetAtFresh K τ⟩
-  | PachnerMove.move23 =>
-      ⟨K.tets.erase τ⟩
-  | PachnerMove.move32 =>
-      ⟨K.tets.erase τ⟩
-
-def applyMove (K : Triangulation) (m : PachnerMove) : Triangulation :=
-  match chooseTet? K with
-  | some τ => applyMoveOnTet K m τ
-  | none => K
-
-def defectPivotVertex? (K : Triangulation) : Option Vertex :=
-  K.vertices.max? fun v => vertexDefect K v
-
-def selectMove (K : Triangulation) : PachnerMove :=
-  match defectPivotVertex? K with
-  | none => PachnerMove.move23
-  | some v =>
-      if h : vertexDegree K v > targetDegree then PachnerMove.move32
-      else if h' : vertexDegree K v < targetDegree then PachnerMove.move14
-      else PachnerMove.move23
+def selectMove (_ : Triangulation) : PachnerMove :=
+  PachnerMove.move23
 
 end Poincare
