d09038a332710a3d204eb59108e801ba1c0ec317
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-03-29 10:38:54 -0300

    Add Mathlib dependency and clean Lake config

--- lakefile.lean
+++ lakefile.lean
@@ -1,12 +1,10 @@
 import Lake
 open Lake DSL
 
-package poincare_new_derivation where
-  leanOptions := #[⟨`pp.unicode.fun, true⟩]
+package «poincare-new-derivation»
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4"
+  "https://github.com/leanprover-community/mathlib4.git"
 
 @[default_target]
-lean_lib Poincare where
-  srcDir := "lean"
+lean_lib Poincare
