ecf63c1f26748e19fd971431787ebce635689941
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-01 09:09:23 -0300

    Fix Mathlib import path for Real to existing module (Defs)

--- lean/Regge/Core.lean
+++ lean/Regge/Core.lean
@@ -1,12 +1,16 @@
 import Regge.So3Concrete
+import Mathlib.Data.Real.Defs
 
 namespace Regge
 
-axiom TetraGeom : Type
-axiom SimplicialComplex : Type
+structure SimplicialComplex where
+  V : Type
+  E : Type
 
+axiom TetraGeom : Type
 axiom FundamentalGroup : SimplicialComplex → Type
 
+-- We use (1 : ℝ) to ensure the instance is found
 axiom detG : TetraGeom → ℝ
 axiom LocalIndependent : TetraGeom → Prop
 
