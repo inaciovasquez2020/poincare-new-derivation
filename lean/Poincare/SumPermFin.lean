700cf3cbe1d7f30d764f24b0145a475a592e6a8e
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-04 16:45:06 -0300

    Replace SumPermFin axiom with finite permutation sum theorem

--- lean/Poincare/SumPermFin.lean
+++ lean/Poincare/SumPermFin.lean
@@ -1,11 +1,19 @@
 import Mathlib
 
-namespace Poincare
-
 open scoped BigOperators
 
-axiom sum_perm_fin
-  {n : ℕ} (f : Fin n → ℤ) (σ : Equiv.Perm (Fin n)) :
-  (∑ i : Fin n, f (σ i)) = ∑ i : Fin n, f i
+namespace Poincare
+
+theorem sum_perm_fin
+{n : ℕ} (f : Fin n → ℤ) (σ : Equiv.Perm (Fin n)) :
+(∑ i : Fin n, f (σ i)) = ∑ i : Fin n, f i := by
+simpa using
+(Fintype.sum_equiv
+σ
+(fun i : Fin n => f (σ i))
+f
+(by
+intro x
+simp))
 
 end Poincare
