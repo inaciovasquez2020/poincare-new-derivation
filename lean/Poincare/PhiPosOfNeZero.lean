481d97da338629a0a948fe7c76c467c784c6d24e
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-04 16:50:19 -0300

    Replace Phi positivity axiom with Nat positivity theorem

--- lean/Poincare/PhiPosOfNeZero.lean
+++ lean/Poincare/PhiPosOfNeZero.lean
@@ -2,7 +2,9 @@ import Poincare.Triangulation
 
 namespace Poincare
 
-axiom phi_pos_of_ne_zero :
-  ∀ T : Triangulation, Phi T ≠ 0 → 0 < Phi T
+theorem phi_pos_of_ne_zero :
+  ∀ T : Triangulation, Phi T ≠ 0 → 0 < Phi T := by
+  intro T h
+  exact Nat.pos_of_ne_zero h
 
 end Poincare
