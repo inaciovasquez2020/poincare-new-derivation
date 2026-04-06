5446b67827d8ca1378dd4037b9008b4114bcdeed
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-06 08:04:43 -0300

    Add explicit Moves frontier marker

---
+++ lean/Poincare/MovesFrontier.lean
@@ -0,0 +1,11 @@
+import Poincare.Moves
+import Poincare.MovesAssumptions
+import Poincare.MovesImplGreedySpec
+
+namespace Poincare
+
+theorem remaining_moves_frontier :
+  True := by
+  trivial
+
+end Poincare
