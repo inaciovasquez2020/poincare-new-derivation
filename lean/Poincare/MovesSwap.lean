1195afe2327e57cb95e328a71c95e26c87e39b49
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-06 07:30:09 -0300

    Refactor MovesSwap to use centralized Moves assumptions

--- lean/Poincare/MovesSwap.lean
+++ lean/Poincare/MovesSwap.lean
@@ -1,12 +1,7 @@
-import Poincare.Moves
-import Poincare.MovesImpl
-import Poincare.MovesSemantics
+import Poincare.MovesAssumptions
 import Poincare.MovesClosure
 
 namespace Poincare
-
-axiom happly_impl : applyMove = applyMoveImpl
-axiom hselect_impl : selectMove = selectMoveImpl
 
 theorem applyMove_spec_from_impl :
   ∀ (K : Triangulation) (m : PachnerMove),
